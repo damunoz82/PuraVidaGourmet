d33a457601e46f29f8fd607744cb192e43ec893d
Daniel Munoz <dmunoz.hon@gmail.com>
2024-02-09 12:35:24 -0600

    testing CI

--- src/main/java/org/puravidatgourmet/security/controllers/UserController.java
+++ src/main/java/org/puravidatgourmet/security/controllers/UserController.java
@@ -3,13 +3,12 @@ package org.puravidatgourmet.security.controllers;
 import org.puravidatgourmet.security.config.security.CurrentUser;
 import org.puravidatgourmet.security.config.security.UserPrincipal;
 import org.puravidatgourmet.security.db.repository.UsuarioRepository;
+import org.puravidatgourmet.security.domain.User;
 import org.puravidatgourmet.security.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import org.puravidatgourmet.security.domain.User;
 
 @RestController
 public class UserController {
@@ -20,6 +19,7 @@ public class UserController {
     @GetMapping("/user/me")
     @PreAuthorize("hasRole('USER')")
     public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        System.out.println("checking...");
         return userRepository.findByEmail(userPrincipal.getName())
                 .orElseThrow(
                 		() -> new ResourceNotFoundException("User", "id", userPrincipal.getName()));
