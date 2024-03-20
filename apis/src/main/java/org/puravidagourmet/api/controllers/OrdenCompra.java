078202125f40594bf5f33614c00a194207ca2c40
pvgourmet <dmunoz.hon@gmail.com>
2024-03-20 16:29:35 -0600

    message socket poc

--- apis/src/main/java/org/puravidagourmet/api/controllers/OrdenCompra.java
+++ apis/src/main/java/org/puravidagourmet/api/controllers/OrdenCompra.java
@@ -1,22 +1,18 @@
 package org.puravidagourmet.api.controllers;
 
-import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import org.puravidagourmet.api.config.security.CurrentUser;
 import org.puravidagourmet.api.config.security.UserPrincipal;
-import org.puravidagourmet.api.domain.entity.Receta;
+import org.puravidagourmet.api.domain.pojo.OrdenPojo;
 import org.puravidagourmet.api.services.RabbitMQService;
 import org.puravidagourmet.api.services.RecetasServices;
-import org.puravidagourmet.api.services.handlers.CocinaStompSessionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 @RestController
 @RequestMapping(path = "/orden-compra", produces = "application/json")
@@ -27,29 +23,18 @@ public class OrdenCompra extends BaseController {
 
   @Autowired private RecetasServices recetasServices;
 
-  //  @Autowired private CocinaSockerConnector connector;
-
-  @Autowired private WebSocketStompClient webSocketStompClient;
+  @Autowired private SimpMessagingTemplate template;
 
   @PostMapping
   @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<String> create(@CurrentUser UserPrincipal userPrincipal)
       throws ExecutionException, InterruptedException {
 
-    Optional<Receta> receta = recetasServices.get(1);
-    if (receta.isPresent()) {
-      rabbitMQService.sendCocinaMessage(receta);
-      rabbitMQService.sendBarMessage(receta);
-      rabbitMQService.sendCompraMessage(receta);
+    OrdenPojo ordenPojo = new OrdenPojo();
+    ordenPojo.setMensaje("Hola Mundo");
 
-      StompSessionHandler stompSessionHandler = new CocinaStompSessionHandler();
-      StompSession stompSession =
-          webSocketStompClient
-              .connectAsync("ws://localhost:8080/ordenes-socket", stompSessionHandler)
-              .get();
-      stompSession.send("/app/cocina/test", "{ message: 'hola mundo #2' }");
-      stompSession.disconnect();
-    }
+    this.template.convertAndSend("/cocina-topic/test", ordenPojo);
+
     return ResponseEntity.created(createLocation(String.valueOf(1))).build();
   }
 }
